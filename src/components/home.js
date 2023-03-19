3f0bb31475de5f6cb0ded9c3915e0d2629cfed89
alexDorni <alexd@diamindsolutions.ca>
2023-03-19 14:04:09 +0200

    merge

--- src/components/home.js
+++ src/components/home.js
@@ -14,6 +14,7 @@
 */
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { ComboboxUsers } from './combobox'
 import {
   BarsArrowUpIcon,
   CheckBadgeIcon,
@@ -27,6 +28,9 @@ import {
   InformationCircleIcon,
 } from '@heroicons/react/20/solid'
 import { Bars3CenterLeftIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { useState } from 'react'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+import { Combobox } from '@headlessui/react'
 
 const navigation = [
   { name: 'Dashboard', href: '#', current: true },
@@ -98,7 +102,32 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
+
+const people = [
+    {
+      id: 1,
+      name: 'Leslie Alexander',
+      imageUrl:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+    // More users...
+  ]
+
+
+
+
 export function Home() {
+
+    const [query, setQuery] = useState('')
+  const [selectedPerson, setSelectedPerson] = useState(null)
+
+  const filteredPeople =
+    query === ''
+      ? people
+      : people.filter((person) => {
+          return person.name.toLowerCase().includes(query.toLowerCase())
+        })
+
   return (
     <>
       {/*
@@ -297,12 +326,56 @@ export function Home() {
                         >
                           Vaccine info
                         </button>
-                        <button
-                          type="button"
-                          className="mt-3 inline-flex items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:ml-3 xl:ml-0 xl:mt-3 xl:w-full"
-                        >
-                          Add patient
-                        </button>
+                        <Combobox as="div" value={selectedPerson} onChange={setSelectedPerson}>
+      {/* <Combobox.Label className="block text-sm font-medium leading-6 text-gray-900">Assigned to</Combobox.Label> */}
+      <div className="relative mt-2">
+        <Combobox.Input
+          className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-12 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          onChange={(event) => setQuery(event.target.value)}
+          displayValue={(person) => person?.name}
+        />
+        <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+          <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+        </Combobox.Button>
+
+        {filteredPeople.length > 0 && (
+          <Combobox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            {filteredPeople.map((person) => (
+              <Combobox.Option
+                key={person.id}
+                value={person}
+                className={({ active }) =>
+                  classNames(
+                    'relative cursor-default select-none py-2 pl-3 pr-9',
+                    active ? 'bg-indigo-600 text-white' : 'text-gray-900'
+                  )
+                }
+              >
+                {({ active, selected }) => (
+                  <>
+                    <div className="flex items-center">
+                      <img src={person.imageUrl} alt="" className="h-6 w-6 flex-shrink-0 rounded-full" />
+                      <span className={classNames('ml-3 truncate', selected && 'font-semibold')}>{person.name}</span>
+                    </div>
+
+                    {selected && (
+                      <span
+                        className={classNames(
+                          'absolute inset-y-0 right-0 flex items-center pr-4',
+                          active ? 'text-white' : 'text-indigo-600'
+                        )}
+                      >
+                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                      </span>
+                    )}
+                  </>
+                )}
+              </Combobox.Option>
+            ))}
+          </Combobox.Options>
+        )}
+      </div>
+    </Combobox>
                       </div>
                     </div>
                     {/* Meta info */}
